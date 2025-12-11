ae8c48de2af8d54d0b02f122da46a56039b8251f
Jenalee Nguyen <jnguy405@ucsc.edu>
2025-12-11 21:04:29 +0000

    deployment tests

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  base: '/cmpm-121-F/',
+  base: '/Mini-Mania/',
   plugins: [react()],
 });
